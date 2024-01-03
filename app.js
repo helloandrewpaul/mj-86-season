8ffaae77a527846097ca6232d8536dbe15ebab7c
helloandrewpaul <andrewpaulstudio@gmail.com>
2024-01-03 02:30:48 -0500

    EOD commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const draw = async () => {
-
-  // Data
-  const dataset = await d3.json('https://www.balldontlie.io/api/v1/stats?seasons[]=1986&player_ids[]=2931').then((data) => data.data);
-  console.log(dataset);
   
-  const xAccessor = d => d.pts
-  const yAccessor = d => d.game.date
+  // Data
+  const dataset = await d3
+    .json('https://www.balldontlie.io/api/v1/stats?seasons[]=1986&player_ids[]=2931').then((data) => data.data);
+  console.log(dataset);
+
+  const xAccessor = (d) => d.pts;
+  const yAccessor = (d) => d.game.date;
 
   // Dimensions
   let dimensions = {
@@ -14,26 +15,29 @@ const draw = async () => {
     margins: 50,
   };
 
-  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
-  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2;
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2;
 
   // Draw Image
-  const svg = d3.select('#chart')
-    .append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height)
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
 
-  const ctr = svg.append("g") // <g>
+  const ctr = svg
+    .append('g') // <g>
     .attr(
-      "transform",
+      'transform',
       `translate(${dimensions.margins}, ${dimensions.margins})`
-    )
+    );
 
   // Scales
-  const yScale = d3.scaleLinear()
+  const yScale = d3
+    .scaleLinear()
     .domain(d3.extent(dataset, yAccessor))
     .range([dimensions.ctrHeight, 0])
-    .nice()
-}
+    .nice();
+};
 
-draw()
+draw();
